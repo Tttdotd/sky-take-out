ddf868b8170d0a53de11f646db7f6366b080a12e
kangkang <rundong.zhang350@gmail.com>
2026-01-22 21:56:42 +0800

    feat: 添加Redis数据库配置

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -15,7 +15,7 @@ public class RedisConfiguration {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始创建redis模板对象...");
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate();
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         //设置连接工厂
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         //String序列化器(key)
